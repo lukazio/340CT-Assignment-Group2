fc2ee9778f81ec2e63fa2db52d79afedfdabf2c1
Lukaz Lim <lukazlim@yahoo.com>
2020-06-11 15:17:43 +0800

    Added stage selection UI TODO: Final Stage background and colours

--- app/src/main/java/com/example/mathrpg/MainActivity.java
+++ app/src/main/java/com/example/mathrpg/MainActivity.java
@@ -14,10 +14,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    //Test code to go into ProfileActivity, can remove after main menu is designed
-    public void onClick_btnTest(View view) {
-        //Intent intent = new Intent(this,ProfileActivity.class);
+    //Test code to go into LoginActivity, can remove after main menu is designed
+    public void onClick_btnTestLogin(View view) {
         Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
+    }
+
+    //Test code to go into StageActivity, can remove after main menu is designed
+    public void onClick_btnTestStage(View view) {
+        Intent intent = new Intent(this,StageActivity.class);
         startActivity(intent);
     }
 }
