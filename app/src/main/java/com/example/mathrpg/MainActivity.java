97ce6bc013ab3e3270898f56eb99863cde1e3181
LeeJunXian89 <58661547+LeeJunXian89@users.noreply.github.com>
2020-06-11 17:36:54 +0800

    Create activity_instructure and some icon

--- app/src/main/java/com/example/mathrpg/MainActivity.java
+++ app/src/main/java/com/example/mathrpg/MainActivity.java
@@ -1,17 +1,30 @@
 package com.example.mathrpg;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button Mbtn_help;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Mbtn_help =(Button)findViewById(R.id.btn_Help);
+
+        Mbtn_help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),Instructure.class);
+                startActivity(i);
+            }
+        });
     }
 
     //Test code to go into ProfileActivity, can remove after main menu is designed
